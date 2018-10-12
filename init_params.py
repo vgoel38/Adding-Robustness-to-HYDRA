cf2f2b29ed55313b593d267ad467f45f5e9e35b9
Vishal Goel <vgoel38@gmail.com>
2018-10-12 13:01:52 +0530

    using numpy now

--- init_params.py
+++ init_params.py
@@ -1,21 +1,21 @@
 import random
 import sys
+import numpy as np
 
 def init_params(layer_in, layer_out):
 #Randomly initialize the parameters of a neural network layer with layer_in
 #incoming connections and layer_out outgoing connections
 
-	epsilon_init = 0.12;
+	epsilon_init = 0.12
 
-	params = []
+	def f(x):
+		return (x * 2 * epsilon_init - epsilon_init)
 
-	for i in range(layer_out):
-		params.append([])
-		#one extra node in the layer_in represents the bias node
-		for j in range(layer_in + 1):
-			params[i].append(random.random() * 2 * epsilon_init - epsilon_init)
+	f = np.vectorize(f)
 
-	return params
+	params = np.random.rand(layer_out,layer_in + 1)
+
+	return f(params)
 
 if __name__ == "__main__":
     print(init_params(int(sys.argv[1]),int(sys.argv[2])))
